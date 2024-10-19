c3b1638023cbdb750fef20ee739d0074acb46dff
BaptisteCtldWbr <baptiste.catelandwambre@gmail.com>
2024-10-19 15:10:14 +0200

    Petites modifs

--- 404.php
+++ 404.php
@@ -10,7 +10,7 @@
 <body>
     <?php require_once('cv-ressources/includes/header.php') ?>
     <main>
-        <h2>erreur 404</h2>
+        <h2>Erreur 404</h2>
         <p><a href="/">Retour à l'accueil</a></p>
     </main>
 </body>
