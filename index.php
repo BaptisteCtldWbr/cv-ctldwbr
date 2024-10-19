2432bafa2030fdd1bfee6cb8a5b23f2f8d465ace
BaptisteCtldWbr <baptiste.catelandwambre@gmail.com>
2024-10-19 13:00:30 +0200

    => php

---
+++ index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV - Baptiste Cateland Wambre</title>
+    <link rel="stylesheet" href="cv-ressources/css/reset.css">
+    <link rel="stylesheet" href="cv-ressources/css/index-header.css">
+    <link rel="stylesheet" href="cv-ressources/css/index-main.css">
+    <link rel="stylesheet" href="cv-ressources/css/index-portfolio.css">
+    <link rel="stylesheet" href="cv-ressources/css/style.css">
+    <link rel="shortcut icon" href="cv-ressources/favicon.png" type="image/x-icon">
+    <meta property="og:title" content="CV - Baptiste Cateland Wambre">
+    <meta property="og:description" content="Découvrez mon profil à travers mon CV et mes expériences">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="http://catelandwambre.alwaysdata.net/cv-ressources/meta-miniature.png">
+</head>
+
+<body>
+    <header>
+        <div id="header-couleur">
+            <section id="moi">
+                <div id="moi-pp">
+                    <img src="cv-ressources/photo.png" alt="">
+                </div>
+                <div id="moi-texte">
+                    <h1>Baptiste Cateland--Wambre</h1>
+                    <p id="moi-titre">Étudiant en développement web</p>
+                    <p id="moi-stage">En recherche de <strong>stage en développement web</strong> à partir du 14 avril
+                        2025 pour une durée minimale de 10 semaines​</p>
+                    <p id="moi-valeurs">Polyvalent • Rigoureux • Ambitieux • Responsable</p>
+                </div>
+            </section>
+            <div id="moi-details">
+                <section id="contact">
+                    <h3>Contact</h3>
+                    <ul id="contact">
+                        <li id="contact-mail"><a href="mailto:baptiste.catelandwambre@gmail.com" target="_blank"><i class="bi-envelope-fill"></i> baptiste.cateland&ZeroWidthSpace;wambre@gmail.com</a></li>
+                        <li><a href="https://www.linkedin.com/in/baptiste-ctldwbr/" target="_blank"><i class="bi-linkedin"></i> /baptiste-ctldwbr</a></li>
+                        <li><a href="https://github.com/BaptisteCtldWbr" target="_blank"><i class="bi-github"></i> / BaptisteCtldWbr</a></li>
+                        <li><a href="https://www.youtube.com/@baptistedrone" target="_blank"><i class="bi-youtube"></i> @baptistedrone</a></li>
+                        <li><a href="https://www.instagram.com/baptiste.drone/" target="_blank"><i class="bi-instagram"></i> @baptiste.drone</a></li>
+                        <li><i class="bi-geo-alt-fill"></i> Paris 13<sup>e</sup></li>
+                    </ul>
+                </section>
+                <div id="competences">
+                    <section id="technologies">
+                        <h3>Technologies</h3>
+                        <div id="techno-flex">
+                            <img src="cv-ressources/techno/html.png" alt="HTML">
+                            <img src="cv-ressources/techno/css.png" alt="CSS">
+                            <img src="cv-ressources/techno/js.png" alt="JS">
+                            <img src="cv-ressources/techno/php.png" alt="PHP ">
+                            <img src="cv-ressources/techno/github.png" alt="Github">
+                            <img src="cv-ressources/techno/wp.png" alt="WordPress">
+                            <img src="cv-ressources/techno/davinci.png" alt="Davinci Resolve">
+                            <img src="cv-ressources/techno/figma.png" alt="Figma">
+                            <img src="cv-ressources/techno/drive.png" alt="Google Drive et sa suite">
+                            <img src="cv-ressources/techno/id.png" alt="Adobe InDesign">
+                            <img src="cv-ressources/techno/ai.png" alt="Adobe Illustrator">
+                            <img src="cv-ressources/techno/ps.png" alt="Photoshop">
+                        </div>
+                    </section>
+                    <section id="langues">
+                        <h3>Langues</h3>
+                        <ul>
+                            <li>&#x1F1EB;&#x1F1F7; Français<meter min="0" max="100" low="33" high="66" optimum="80" value="100">Langue maternelle</meter>
+                            </li>
+                            <li>&#x1F1EC;&#x1F1E7; Anglais<meter min="0" max="100" low="33" high="66" optimum="80" value="70">Langue maternelle</meter></li>
+                            <li>&#x1F1EA;&#x1F1F8; Espagnol<meter min="0" max="100" low="33" high="66" optimum="80" value="34">Niveau débutant</meter></li>
+                        </ul>
+                    </section>
+                </div>
+                <section id="interets">
+                    <h3>Centres d'intérêt</h3>
+                    <ul>
+                        <li>&#x1F6F8; Drone (Stabilisé et FPV)</li>
+                        <li>&#x1F3AC; Audiovisuel / Court-métrages</li>
+                        <li>&#x1F4F7; Photographie</li>
+                        <li>&#x2328;&#xFE0F; Montage</li>
+                        <li>&#x1F9ED; Voyage</li>
+                    </ul>
+                </section>
+            </div>
+            <div id="scroll">
+                <a href="#contenu" class="sans">
+                    <p>></p>
+                </a>
+            </div>
+        </div>
+    </header>
+    
+
+
+    <main id="contenu">
+        
+        <nav>
+            <ul>
+                <li class="nav"><a class="sans" href="#formations">Expériences</a></li>
+                <li class="nav"><a class="sans" href="#projets">Projets</a></li>
+                <li class="nav"><a class="sans" href="#portfolio">Portfolio</a></li>
+            </ul>
+        </nav>
+
+        <p id="lien-cv">Accéder au CV au format PDF : <a href="cv-pdf.html" target="_blank">cv-catelandwambre.pdf</a><i class="bi-filetype-pdf"></i></p>
+        
+        <section id="formations">
+            <h2>Expériences et Formations</h2>
+            <article class="exp">
+                <img src="cv-ressources/exp/cyiut.png" alt="Logo IUT de Cergy Pontoise" class="exp-img">
+                <div class="exp-text">
+                    <h4>BUT Métiers du Multimédia et de l'Internet <span class="exp-det">IUT de Cergy Pontoise</span>
+                    </h4>
+                    <p class="exp-date">Septembre 2023 à juillet 2026</p>
+                    <p class="exp-desc">Bachelor Universitaire de Technologie - Multimédia et Métiers de l’Internet, parcours <strong>Développement Web et Dispositifs Interactifs</strong> à l’IUT de Cergy Pontoise – En cours de préparation.</p>
+                    <p class="exp-comp">Développement Web, Création numérique, Audiovisuel et Communication avec Gestion de projet...</p>
+                </div>
+            </article>
+            <article class="exp">
+                <img src="cv-ressources/exp/llg.png" alt="Logo Lycée Louis le Grand" class="exp-img">
+                <div class="exp-text">
+                    <h4>Baccalauréat STI2D <span class="exp-det">Lycée Louis le Grand</span></h4>
+                    <p class="exp-date">Septembre 2021 à juillet 2023, Paris 5</p>
+                    <p class="exp-desc">Baccalauréat Sciences et Technologie de l’Ingénierie et du Développement Durable au Lycée Louis le Grand option Systèmes Informatiques et Numériques – Mention très bien</p>
+                    <p class="exp-comp">Ingénieurie, Informatique, Mathématiques, Physique</p>
+                </div>
+            </article>
+            <article class="exp">
+                <img src="cv-ressources/exp/taf.png" alt="Travail" class="exp-img">
+                <div class="exp-text">
+                    <h4>Travail en indépendant</h4>
+                    <p class="exp-date">Septembre 2021 à Aujourd'hui</p>
+                    <p class="exp-desc">Service à la personne, du babysitting, de l’événementiel ou de la vente, déménagement</p>
+                </div>
+            </article>
+        </section>
+        <section id="projet">
+            <h2 id="projets">Expériences et projets</h2>
+            <article class="exp">
+                <img src="cv-ressources/exp/cv-web.png" alt="CV sur le web" class="exp-img">
+                <div class="exp-text">
+                    <h4>Mon CV en HTML</h4>
+                    <p class="exp-desc">Développement de mon CV en HTML CSS</p>
+                    <p class="exp-comp">HTML - CSS - GitHub - Hébergement</p>
+                </div>
+            </article>
+            <article class="exp">
+                <img src="cv-ressources/exp/sae105.png" alt="Illustration de ce site" class="exp-img">
+                <div class="exp-text">
+                    <h4>Développement en équipe d'un site web <span class="exp-det">Projet universitaire</span></h4>
+                    <p class="exp-date">Décembre 2023 et mai 2024</p>
+                    <p class="exp-desc">Développement en équipe d'un site web dans le cadre d'un projet universitaire
+                    </p>
+                    <p class="exp-comp">HTML - CSS - PHP - MySql - GitHub - Hébergement - Travail en équipe</p>
+                    <p class="exp-lien"><a href="https://asaf-font.alwaysdata.net/sae203/code" target="_blank">Site web
+                        hébergé</a> - <a href="https://github.com/BaptisteCtldWbr/SAE203" target="_blank">Dépôt
+                            Github</a></p>
+                        </div>
+                    </article>
+            <article class="exp">
+                <img src="cv-ressources/exp/ahuana.png" alt="Logo " class="exp-img">
+                <div class="exp-text">
+                    <h4>Protectyo Andina Rumicruz <span class="exp-det">Scouts et Guides de France</span></h4>
+                    <p class="exp-date">Été 2024, préparation depuis septembre 2023, témoignage jusqu'en 2025</p>
+                    <p class="exp-desc">Projet scout de solidarité et rencontre internationale en Équateur, en partenariat avec <a href="https://ahuana.com" target="_blank">Ahuana</a>, avec le soutien de <a href="https://france-volontaires.org" target="_blank">France Volontaires</a>, dans le cadre de la proposition <a href="https://compagnons.sgdf.fr/experiments-a-letranger/" target="_blank">compagnons</a>.</p>
+                    <p class="exp-comp">Gestion de projet et d'équipe - Recherche de financements - Gestion et suivi de budget - Rédaction d'un dossier de camp<br>Mise en place de partenariat - Solidarité internationale - Rencontre internationale - Réalisation d'un documentaire</p>
+                    <p class="exp-lien"><a href="https://drive.google.com/file/d/1BYYX7KkHGxkjYFnbCGxCnruPDGyg1EVy/view?usp=drive_link" target="_blank">Dossier d'information - subvention</a> - <a href="https://compart-ou.alwaysdata.net/ahuana" target="_blank">Page de présentation</a></p>
+                </div>
+            </article>
+            <article class="exp">
+                <img src="cv-ressources/exp/drone.png" alt="Logo baptiste.drone" class="exp-img">
+                <div class="exp-text">
+                    <h4>Réalisation de cinématiques au drone</h4>
+                    <p class="exp-date">2022 à Aujourd'hui</p>
+                    <p class="exp-desc">Par passion, je réalise des cinématiques avec mon drone. Majoritairement de par moi-même, mais aussi pour des court-métrages.</p>
+                    <p class="exp-comp">Pilotage de drone stabilisé - Pilotage de drone FPV - Montage Vidéo (DaVinci
+                        Resolve) - Étalonnage - Sound Design</p>
+                        <p class="exp-lien"><a href="https://www.youtube.com/@baptistedrone" target="_blank">Chaîne
+                            YouTube</a> - <a href="https://www.instagram.com/baptiste.drone/" target="_blank">Compte
+                            Instagram</a> - <a
+                            href="https://www.behance.net/gallery/199966985/baptistedrone-cinmatiques"
+                            target="_blank">Projet Behance</a></p>
+                </div>
+            </article>
+            <article class="exp">
+                <img src="cv-ressources/exp/talents.png" alt="Logo du festival des talents de l'IUT" class="exp-img">
+                <div class="exp-text">
+                    <h4>Communication pour un festival <span class="exp-det">Projet universitaire</span></h4>
+                    <p class="exp-date">Novembre 2023 à mai 2024</p>
+                    <p class="exp-desc">Élaboration et mise en place d’une stratégie de communication</p>
+                    <p class="exp-comp">Bases de la communication - Gestion de projet - Réalisation de pastilles de
+                        communication - Réalisation d'affiches - Rédaction de contenus</p>
+                        <a href="https://www.behance.net/gallery/199938091/Promotion-dun-festival" target="_blank">Pastilles
+                        de communication et Affiches (Behance)</a>
+                    </div>
+                </article>
+            </section>
+            
+            <section id="portfolio">
+                <h2>Portflio</h2>
+                <div class="cont-projet">
+                    <article class="projet univ" style="background-image: url('cv-ressources/img-portfolio/cv-portfolio.png');" title="CV / Portflio : Afin de présenter mon profil et mes atouts, j'ai conçu et développé ce petit site web. Vous pouvez retrouver dessus mon CV : mes expériences et mon contact, avec mon portfolio : les projets que j'ai réalisé récemment."><!--cadre du projet ///// image projet ///// {Titre} : {Description} ({Date})--><a class="sans" href="projets.html"><!--Lien Projet-->
+                        <h4 class="titre-projet">CV / Portfolio</h4>        <!--Nom Projet-->
+                        <ul>                                                <!--Tags Projet-->
+                            <li><i class="bi-filetype-html"></i></li>
+                            <li><i class="bi-filetype-css"></i></li>
+                            <li><i class="bi-filetype-php"></i></li>
+                            <li><i class="bi-filetype-sql"></i></li>
+                            <li><i class="bi-github"></i></li>
+                        </ul>
+                    </a></article>
+                    <article class="projet scout" style="background-image: url('cv-ressources/img-portfolio/cv-portfolio.png');" title="CV / Portflio : Afin de présenter mon profil et mes atouts, j'ai conçu et développé ce petit site web. Vous pouvez retrouver dessus mon CV : mes expériences et mon contact, avec mon portfolio : les projets que j'ai réalisé récemment."><!--image projet ///// {Titre} : {Description} ({Date})--><a class="sans" href="projets.html"><!--Lien Projet-->
+                        <h4 class="titre-projet">CV / Portfolio</h4>        <!--Nom Projet-->
+                        <ul>                                                <!--Tags Projet-->
+                            <li><i class="bi-filetype-html"></i></li>
+                            <li><i class="bi-filetype-css"></i></li>
+                            <li><i class="bi-filetype-php"></i></li>
+                            <li><i class="bi-filetype-sql"></i></li>
+                            <li><i class="bi-github"></i></li>
+                        </ul>
+                    </a></article>
+                    <article class="projet audiovisuel" style="background-image: url('cv-ressources/img-portfolio/cv-portfolio.png');" title="CV / Portflio : Afin de présenter mon profil et mes atouts, j'ai conçu et développé ce petit site web. Vous pouvez retrouver dessus mon CV : mes expériences et mon contact, avec mon portfolio : les projets que j'ai réalisé récemment."><!--image projet ///// {Titre} : {Description} ({Date})--><a class="sans" href="projets.html"><!--Lien Projet-->
+                        <h4 class="titre-projet">CV / Portfolio</h4>        <!--Nom Projet-->
+                        <ul>                                                <!--Tags Projet-->
+                            <li><i class="bi-filetype-html"></i></li>
+                            <li><i class="bi-filetype-css"></i></li>
+                            <li><i class="bi-filetype-php"></i></li>
+                            <li><i class="bi-filetype-sql"></i></li>
+                            <li><i class="bi-github"></i></li>
+                        </ul>
+                    </a></article>
+                </div>
+            </section>
+            <section id="contact-form">
+                <h2>Me contacter</h2>
+                <form action="#" method="post">
+                    <div class="contact-champs" id="div-nom">
+                        <label for="nom" class="required">Votre nom</label>
+                        <input type="text" name="nom" id="nom" placeholder="Prénom Nom..." required>
+                    </div>
+                    <div class="contact-champs" id="div-mail">
+                        <label for="mail" class="required">Votre mail</label>
+                        <input type="email" name="mail" id="mail" placeholder="hello@exemple.fr" required>
+                    </div>
+                    <div class="contact-champs" id="div-msg">
+                        <label for="msg" class="required">Votre message</label>
+                        <textarea name="msg" id="msg" placeholder="Bonjour, je souhaite vous contacter pour ..." required></textarea>
+                    </div>
+                    <button type="submit">Envoyer</button>
+                </form>
+            </section>
+        </main>
+        <div style="height: 200px;" id="bas"></div>
+        <a class="haut" href="#" title="Retourner en haut de la page"><i class="bi-arrow-up"></i></a>
+    </body>
+    </html>
