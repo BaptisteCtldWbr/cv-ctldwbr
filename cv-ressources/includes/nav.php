d86dcbf9c5b9df40b5e5522d4d10851e9c9167d7
BaptisteCtldWbr <baptiste.catelandwambre@gmail.com>
2024-10-25 14:33:19 +0200

    Corrections

--- cv-ressources/includes/nav.php
+++ cv-ressources/includes/nav.php
@@ -1,7 +1,8 @@
 <nav>
     <ul>
-        <li class="nav"><a class="sans" href="/index.php#formations">Expériences</a></li>
-        <li class="nav"><a class="sans" href="/index.php#projets">Projets</a></li>
-        <li class="nav"><a class="sans" href="/index.php#portfolio">Portfolio</a></li>
+        <li class="nav"><a class="sans" href="index.php#formations">Expériences</a></li>
+        <!--<li class="nav"><a class="sans" href="index.php#projets">Projets</a></li>-->
+        <li class="nav"><a class="sans" href="index.php#portfolio">Portfolio</a></li>
+        <li class="nav"><a class="sans" href="cv-pdf.html" target="_blank">CV PDF</a></li>
     </ul>
 </nav>
