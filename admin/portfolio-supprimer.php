ca6c7f7c6ef35a96860177bd342803c1bdcb6fd8
BaptisteCtldWbr <baptiste.catelandwambre@gmail.com>
2024-11-16 02:58:08 +0100

    ADMIN - Supprimer projet

---
+++ admin/portfolio-supprimer.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('../cv-ressources/includes/connexion-bdd.php');
+
+$projet = $_GET['id'];
+
+$delete = "DELETE FROM portfolio WHERE `portfolio`.`id` = {$projet}";
+$result = mysqli_query($lien, $delete);
+
+if($result){
+    header("Location : portfolio.php?msg=suppr-validee");
+} else {
+    header("Location : portfolio.php?msg=suppr-ratee");
+}
+
+?>
