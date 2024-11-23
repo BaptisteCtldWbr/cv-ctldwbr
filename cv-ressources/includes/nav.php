dcab9ca21ef7f176c5541216cd9774455737d7b9
BaptisteCtldWbr <baptiste.catelandwambre@gmail.com>
2024-11-24 00:14:24 +0100

    Ajout Contact à la nav

--- cv-ressources/includes/nav.php
+++ cv-ressources/includes/nav.php
@@ -1,8 +1,8 @@
 <nav>
     <ul>
         <li class="nav"><a class="sans" href="index.php#formations">Expériences</a></li>
-        <!--<li class="nav"><a class="sans" href="index.php#projets">Projets</a></li>-->
         <li class="nav"><a class="sans" href="index.php#portfolio">Portfolio</a></li>
+        <li class="nav"><a class="sans" href="index.php#contact-form">Contact</a></li>
         <li class="nav"><a class="sans" href="cv-ressources/cv-catelandwambre.pdf" download="CATELANDWAMBRE-CV.pdf">CV PDF</a></li>
     </ul>
 </nav>
