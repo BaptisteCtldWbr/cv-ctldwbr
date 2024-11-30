37db4f330722748725b5f58bfb163ca218e4035f
BaptisteCtldWbr <baptiste.catelandwambre@gmail.com>
2024-11-30 16:42:40 +0100

    Ajout posts à la navigation

--- cv-ressources/includes/nav.php
+++ cv-ressources/includes/nav.php
@@ -3,6 +3,7 @@
         <li class="nav"><a class="sans" href="index.php#details">CV</a></li>
         <li class="nav"><a class="sans" href="index.php#portfolio">Portfolio</a></li>
         <li class="nav"><a class="sans" href="index.php#contact-form">Contact</a></li>
+        <li class="nav"><a class="sans" href="index.php#linkedin">Posts</a></li>
         <li class="nav"><a class="sans" href="cv-ressources/cv-catelandwambre.pdf" download="CATELANDWAMBRE-CV.pdf">CV PDF</a></li>
     </ul>
 </nav>
